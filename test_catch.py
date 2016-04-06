0ee375fbe3b2ee85263a54f26f120e168be8b8c2
Fariz Rahman <farizrahman4u@gmail.com>
2016-04-07 01:53:37 +0530

    add example

---
+++ test_catch.py
@@ -0,0 +1,22 @@
+from keras.models import Sequential
+from qlearn import QLearn
+from keras.layers import *
+from games import Catch
+from keras.optimizers import *
+from agent import Agent
+
+grid_size = 10
+hidden_size = 100
+nb_frames = 1
+
+model = Sequential()
+model.add(Flatten(input_shape=(nb_frames, grid_size, grid_size)))
+model.add(Dense(hidden_size, activation='relu'))
+model.add(Dense(hidden_size, activation='relu'))
+model.add(Dense(3))
+model.compile(sgd(lr=.2), "mse")
+
+catch = Catch(grid_size)
+agent = Agent(model=model)
+agent.train(catch, batch_size=10, nb_epoch=1000, epsilon=.1)
+agent.play(catch)
